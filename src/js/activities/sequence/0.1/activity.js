b6f7d15635dfa4bb9f44ac5a398260f98bf57e6d
Gueldre Jean-François <jfgueldre@hotmail.com>
2020-12-10 15:06:10 +0100

    .

--- src/js/activities/sequence/0.1/activity.js
+++ src/js/activities/sequence/0.1/activity.js
@@ -1,13 +1,15 @@
 Vue.component('sequence', {
     template: `<div>
-    <div style="border:1px solid #CAC8C7; min-height:20px; margin:5px; margin-bottom:10px">
+    <div style="border:1px solid #CAC8C7; min-height:20px;background-color:white; min-width:500px">
         <activityHeader :title="definition.title" icon="img/move.svg" color="#0077FF"></activityHeader>
-        <draggable v-model="activities" item-key="id" v-bind="dragOptions" style="min-height:20px;background-color:white">
-            <component v-for="element in activities"
-                            :key="element.id"
-                            :is="element.type"
-                            v-model="element">
-            </component>
+        <draggable v-model="activities" item-key="id" v-bind="dragOptions" style="min-height:20px;background-color:white; padding:5px">
+            <div v-for="(element,index) in activities" :key="element.id">
+                <component :key="element.id"
+                                :is="element.type"
+                                v-model="element">
+                </component>
+                <img v-if="index != activities.length - 1" style="height:35px; background-color:white; opacity:0.8; width:100%" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4NTktMSI/Pg0KPCEtLSBHZW5lcmF0b3I6IEFkb2JlIElsbHVzdHJhdG9yIDE5LjAuMCwgU1ZHIEV4cG9ydCBQbHVnLUluIC4gU1ZHIFZlcnNpb246IDYuMDAgQnVpbGQgMCkgIC0tPg0KPHN2ZyB2ZXJzaW9uPSIxLjEiIGlkPSJMYXllcl8xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB4PSIwcHgiIHk9IjBweCINCgkgdmlld0JveD0iMCAwIDQ3Ni4yMTMgNDc2LjIxMyIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgNDc2LjIxMyA0NzYuMjEzOyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+DQo8cG9seWdvbiBwb2ludHM9IjM0Ny41LDMyNC4zOTMgMjUzLjM1Myw0MTguNTQxIDI1My4zNTMsMCAyMjMuMzUzLDAgMjIzLjM1Myw0MTkuMDMzIDEyOC43MTMsMzI0LjM5MyAxMDcuNSwzNDUuNjA3IA0KCTIzOC4xMDcsNDc2LjIxMyAzNjguNzEzLDM0NS42MDYgIi8+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8L3N2Zz4NCg=="/>
+            </div>
         </draggable>
 </div>
     </div>`,
--- src/js/designsurface.js
+++ src/js/designsurface.js
@@ -2,12 +2,18 @@ var coreflow = {
     designSurface : function (elementPath, opts, definitionObj) {
         var app = new Vue({
             el: elementPath,
-            template: `<div class="cs-cf" v-cloak style="background-color:#EAEAEA">
-                            <component 
-                                :key="definition.activity.id"
-                                :is="definition.activity.type"
-                                v-model="definition.activity">
-                            </component>
+            template: `<div v-cloak style="background-color:#EAEAEA;width:100%;height:100%; position:absolute">
+                            <div style="display:Flex;justify-content: center;align-items: center;">
+                                <draggable v-model="definition.activities" item-key="id" style="min-height:20px; padding:5px">
+                                    <div v-for="(element,index) in definition.activities" :key="element.id">
+                                        <component :key="element.id"
+                                                        :is="element.type"
+                                                        v-model="element">
+                                        </component>
+                                        <img v-if="index != definition.activities.length - 1" style="height:35px;  opacity:0.8; width:100%; bottom:0px" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4NTktMSI/Pg0KPCEtLSBHZW5lcmF0b3I6IEFkb2JlIElsbHVzdHJhdG9yIDE5LjAuMCwgU1ZHIEV4cG9ydCBQbHVnLUluIC4gU1ZHIFZlcnNpb246IDYuMDAgQnVpbGQgMCkgIC0tPg0KPHN2ZyB2ZXJzaW9uPSIxLjEiIGlkPSJMYXllcl8xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB4PSIwcHgiIHk9IjBweCINCgkgdmlld0JveD0iMCAwIDQ3Ni4yMTMgNDc2LjIxMyIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgNDc2LjIxMyA0NzYuMjEzOyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+DQo8cG9seWdvbiBwb2ludHM9IjM0Ny41LDMyNC4zOTMgMjUzLjM1Myw0MTguNTQxIDI1My4zNTMsMCAyMjMuMzUzLDAgMjIzLjM1Myw0MTkuMDMzIDEyOC43MTMsMzI0LjM5MyAxMDcuNSwzNDUuNjA3IA0KCTIzOC4xMDcsNDc2LjIxMyAzNjguNzEzLDM0NS42MDYgIi8+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8L3N2Zz4NCg=="/>
+                                    </div>
+                                </draggable>
+                            </div>
                     </div>`,
                 data: function () {
                     return {

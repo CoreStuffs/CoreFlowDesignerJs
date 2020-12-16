258802308ebf7cde1d4ca70b6414ca3125a6ee35
Gueldre Jean-François <jfgueldre@hotmail.com>
2020-12-16 09:21:19 +0100

    .

--- src/js/activities/parallel/0.1/activity.js
+++ src/js/activities/parallel/0.1/activity.js
@@ -10,18 +10,18 @@ Vue.component('parallel', {
                     </div>
                     <div style="display:flex; margin:0 auto;margin-top:-15px" :style="{'width' : (activities.length * 420) + 'px' }">
                             <div style="width:420px;" v-for="(element,index) in activities" :key="element.id">
-                                <div style="height:35px;  opacity:0.8; width:100%; bottom:0px">
-                                    <svg version="1.1" id="Layer_1" height="35px" width="100%" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                <div style="opacity:0.8; width:100%; bottom:0px">
+                                    <svg version="1.1" id="Layer_1" height="25px" width="100%" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                                             xml:space="preserve">
                                         <!-- Left -->
-                                        <line id="svg_1" y2="10" x2="100%" y1="10" x1="50%" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="2" stroke="#000000" fill="none"/>
-                                        <!-- Middle (but not single) -->
-                                        <line id="svg_1" y2="10" x2="100%" y1="10" x1="0%" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="2" stroke="#000000" fill="none"/>
+                                        <line v-if="index===0" id="svg_1" y2="1" x2="100%" y1="1" x1="50%" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="2" stroke="#000000" fill="none"/>
                                         <!-- Right -->
-                                        <line id="svg_1" y2="10" x2="100%" y1="10" x1="0%" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="2" stroke="#000000" fill="none"/>
+                                        <line v-else-if="activities.length===index+1" id="svg_1" y2="1" x2="50%" y1="1" x1="0%" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="2" stroke="#000000" fill="none"/>
+                                        <!-- Middle (but not single) -->
+                                        <line v-else-if="activities.length!=1" id="svg_1" y2="1" x2="100%" y1="1" x1="0%" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="2" stroke="#000000" fill="none"/>
 
 
-                                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="9" height="27" viewBox="0 0 17 25" xml:space="preserve">
+                                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0" height="25" viewBox="0 0 17 25" xml:space="preserve">
                                             <polyline  stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="2" stroke="#000000" fill="none" points="1,16 8.5,23 16,16"/>
                                             <line  y2="0" x2="8.5" y1="23" x1="8.5" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="2" stroke="#000000" fill="none"/>
                                         </svg>
